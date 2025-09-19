3391167cf93ba7008fb5f2863cd46d685208e816
megha kumari <megha99734@gmail.com>
2025-09-19 22:54:49 +0530

    style: remove extra whitespace in page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -338,6 +338,7 @@ export default function Home() {
         </div>
       </section>
 
+
       {/* Footer */}
       <footer className="bg-gray-900 text-white py-12">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
